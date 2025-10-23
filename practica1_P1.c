7ca0996edbd8a6b45a17c7d57893fb4608436a90
Marto-kun <martinvozme456@gmail.com>
2025-10-23 09:02:58 -0500

    Intentos

--- practica1_P1.c
+++ practica1_P1.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    int cursos;
+    int i = 4;
+    int xd[3] = {3,2,4};
 
-    printf("Hola, mundo!");
+    do
+    {
+        printf("\nIngrese la cantidad de cursos ofrecidos: ");
+        scanf("%d", &cursos);
+    } while (cursos <= 0);
+
+    //const int n = cursos;
+    int cupos[i];
+    //int estudiantesIns[n];
+    //float costos[n];
+    printf("%i",xd);
+    printf("%d",cursos);
     return 0;
 }
