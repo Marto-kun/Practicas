e02acc586b433fa2420fa7556c627dcb2bb92e58
Marto-kun <martinvozme456@gmail.com>
2025-10-28 12:25:44 -0500

    Funcion validacion

--- practica2_P1.c
+++ practica2_P1.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 
+bool ValidacionCaracteres(int precio[5], int i)
+{
 
-bool ValidacionCaracteres(int precio){
-    if (precio < ){
-
+    if (!isdigit(precio[i]))
+    {
+        return false;
+    }
+    else
+    {
+        return true;
     }
 }
 
@@ -20,16 +27,20 @@ int main(int argc, char *argv[])
         fgets(nombre[i], sizeof(nombre), stdin);
     }
 
-
     for (int i = 0; i < 5; i++)
     {
+        bool validacion;
         do
         {
-            printf("Ingrese el precio de %s: ", nombre[i]);
-            scanf("%i", &precio);
+            do
+            {
+                printf("Ingrese el precio de %s: ", nombre[i]);
+                scanf("%i", &precio);
+                validacion = ValidacionCaracteres(precio, i);
+            } while (validacion == false);
+
         } while (precio <= 0);
     }
-
 
     for (int i = 0; i < 5; i++)
     {
